4cee333dedff8dd5fe9dca4ff9ab1e637f2b2430
goalryan <goalryan@qq.com>
2017-10-24 19:57:56 +0800

    搜索框样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,114 @@
-//index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    barcode: "",
+    hiddenLoading: true,
+    hiddenData: true,
+    hiddenDropdown: true,
+    hiddenClear: true,
+    demoData: 'XXXX',
+    Product: {},
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  bindBarcodeInput: function (e) {
+    this.setData({
+      barcode: e.detail.value
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  bindBarcodeFocus: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hiddenDropdown: false,
+      hiddenClear: false
+    })
+  },
+  bindBarcodeBlur: function (e) {
+    this.setData({
+      hiddenDropdown: true,
+      hiddenClear: true
+    })
+  },
+  scan: function (e) {
+    var that = this;
+    wx.scanCode({
+      success: function (res) {
+        that.setData({
+          barcode: res.result
+        });
+        that.query(e);
+      },
+      fail: function () {
+        that.setData({
+          barcode: "",
+          hiddenData: true
+        });
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  setDemoData: function (e) {
+    this.setData({
+      barcode: this.data.demoData
+    });
+  },
+  clear: function (e) {
+    this.setData({
+      barcode: "",
+      hiddenData: true
+    });
+  },
+  query: function (e) {
+    var url = "https://www.xxx.com/query";//查询数据的URL
+    var that = this;
+    if (that.data.barcode == undefined
+      || that.data.barcode == null
+      || that.data.barcode.length <= 0) {
+      that.setData({ hiddenData: true });
+      wx.showToast({
+        title: '请输入条码',
+        image: '/images/fail.png',
+        duration: 2000
+      });
+      return;
+    }
+    wx.request({
+      url: url,
+      data: { barcode: that.data.barcode },
+      method: 'GET',
+      success: function (res) {
+        var result = res.data;
+        if (result.Status != 0) {
+          that.setData({ hiddenData: true });
+          wx.showToast({
+            title: result.Message,
+            image: '/images/fail.png',
+            duration: 2000
+          })
+          return;
+        }
+        that.setData({ Product: result.Data, hiddenData: false });
+        wx.showToast({
+          title: "获取数据成功",
+          image: '/images/ok.png',
+          duration: 2000
+        })
+      },
+      fail: function (e) {
+        var toastText = '获取数据失败' + JSON.stringify(e);
+        that.setData({
+          hiddenLoading: !that.data.hiddenLoading,
+          hiddenData: true
+        });
+        wx.showToast({
+          title: toastText,
+          icon: '',
+          duration: 2000
+        })
+      },
+      complete: function () {
+        // complete
+      }
     })
   }
 })
